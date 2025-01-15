37a0a0e48727237413f8f261d0727bdb1a26a7f7
Oliver Säfström <oliver.safstrom@mobilityguard.com>
2025-01-15 07:33:39 +0100

    Delete old sessions once a day

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #[cfg(feature = "ssr")]
+use leptos::prelude::*;
+#[cfg(feature = "ssr")]
 #[tokio::main]
 async fn main() {
     use axum::Router;
@@ -9,6 +11,8 @@ async fn main() {
     use leptos_axum::{generate_route_list, LeptosRoutes};
     extern crate dotenv;
     use dotenv::dotenv;
+    use tokio_cron_scheduler::Job;
+    use tokio_cron_scheduler::JobScheduler;
     use tower_cookies::CookieManagerLayer;
 
     dotenv().ok();
@@ -20,6 +24,34 @@ async fn main() {
     let routes = generate_route_list(App);
 
     init_db().await.expect("Unable to init db");
+
+    let sched = JobScheduler::new()
+        .await
+        .expect("could not create scheduler");
+    // Add async job to clear db
+    sched
+        .add(
+            Job::new_async("0 0 * * * *", |uuid, mut l| {
+                Box::pin(async move {
+                    log!("Running scheduled job for cleaning up db");
+                    if let Err(err) = delete_old_sessions().await {
+                        log!("Failed to delete old sessions{}", err);
+                    }
+
+                    // Query the next execution time for this job
+                    let next_tick = l.next_tick_for_job(uuid).await;
+                    match next_tick {
+                        Ok(Some(ts)) => log!("Next time for job is {:?}", ts),
+                        _ => println!("Could not get next tick for job"),
+                    }
+                })
+            })
+            .expect("unable to create async job"),
+        )
+        .await
+        .expect("unable to create scheduled job");
+
+    sched.start().await.expect("unable to start scheduler");
 
     let app = Router::new()
         .leptos_routes(&leptos_options, routes, {
@@ -37,6 +69,35 @@ async fn main() {
         .unwrap();
 }
 
+#[cfg(feature = "ssr")]
+async fn delete_old_sessions() -> Result<(), ServerFnError> {
+    use gubbhockey::database::get_db;
+    use leptos::logging::log;
+
+    let pool = get_db();
+
+    log!("Deleting old sessions");
+    match sqlx::query!(
+        r#"
+        DELETE FROM session
+        WHERE expires_at <= NOW()
+        "#,
+    )
+    .execute(pool)
+    .await
+    {
+        Ok(_) => {
+            log!("Sessions deleted successfully.");
+            Ok(())
+        }
+        Err(e) => {
+            log!("Database error: {:?}", e);
+            Err(ServerFnError::ServerError(
+                "Failed to delete old sessions.".to_string(),
+            ))
+        }
+    }
+}
 #[cfg(not(feature = "ssr"))]
 pub fn main() {
     // no client-side main function
