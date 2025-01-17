ef8ff5c39dc8b2e3f5a227eb55f122f6c1a5eee0
Oliver Säfström <oliver.safstrom@mobilityguard.com>
2025-01-17 12:36:09 +0100

    Add workflow and dockerfile

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+# Get started with a build env with Rust nightly
+FROM rustlang/rust:nightly-alpine as builder
+
+RUN apk update && \
+    apk add --no-cache bash curl npm libc-dev binaryen openssl-dev clang libclang build-base
+
+RUN npm install -g sass
+
+RUN curl --proto '=https' --tlsv1.2 -LsSf https://github.com/leptos-rs/cargo-leptos/releases/latest/download/cargo-leptos-installer.sh | sh
+
+# Add the WASM target
+RUN rustup target add wasm32-unknown-unknown
+
+WORKDIR /work
+COPY . .
+
+RUN cargo update -p wasm-bindgen --precise 0.2.100
+RUN cargo leptos build --release -vv
+
+FROM rustlang/rust:nightly-alpine as runner
+
+WORKDIR /app
+
+COPY --from=builder /work/target/release/gubbhockey /app/
+COPY --from=builder /work/target/site /app/site
+COPY --from=builder /work/Cargo.toml /app/
+
+EXPOSE 3000
+ENV RUST_LOG="info"
+ENV LEPTOS_SITE_ADDR="0.0.0.0:3000"
+ENV LEPTOS_SITE_ROOT=./site
+
+CMD ["/app/gubbhockey"]
---
+++ migrations/20250115194728_createdb.sql
@@ -0,0 +1,45 @@
+-- Players Table
+CREATE TABLE IF NOT EXISTS Player (
+    player_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    given_name VARCHAR(50) NOT NULL,
+    family_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    access_group VARCHAR(20) NOT NULL
+);
+
+-- Gamedays Table
+CREATE TABLE IF NOT EXISTS Gameday (
+    gameday_id SERIAL PRIMARY KEY,
+    start_date TIMESTAMPTZ NOT NULL,
+    end_date TIMESTAMPTZ NOT NULL
+);
+
+-- Join Table for Player-Gameday relationship
+CREATE TABLE IF NOT EXISTS Player_Gameday (
+    player_id INT NOT NULL,
+    gameday_id INT NOT NULL,
+    PRIMARY KEY (player_id, gameday_id),
+    FOREIGN KEY (player_id) REFERENCES Player(player_id) ON DELETE CASCADE,
+    FOREIGN KEY (gameday_id) REFERENCES Gameday(gameday_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS pkce_store (
+    id SERIAL PRIMARY KEY, 
+    csrf_token TEXT NOT NULL,                     -- CSRF token associated with the request
+    pkce_verifier TEXT NOT NULL,                  -- PKCE verifier value
+    created_at TIMESTAMPTZ DEFAULT NOW(),           -- Timestamp of creation
+    expires_at TIMESTAMPTZ NOT NULL                 -- Expiration timestamp
+);
+
+-- Index to quickly look up by CSRF token
+CREATE UNIQUE INDEX idx_csrf_token ON pkce_store (csrf_token);
+
+CREATE TABLE IF NOT EXISTS Session (
+    session_id UUID PRIMARY KEY,                  -- Unique session ID
+    player_id INT NOT NULL,                       -- Associated player ID
+    created_at TIMESTAMPTZ DEFAULT NOW(),         -- Creation timestamp
+    expires_at TIMESTAMPTZ NOT NULL,              -- Expiration timestamp
+
+    FOREIGN KEY (player_id) REFERENCES Player(player_id) ON DELETE CASCADE
+);
